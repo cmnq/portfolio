f5150b72a26be69b1acf30ce8be8ddd2a2b13302
cmnq <kpodufalski@gmail.com>
2019-05-15 20:10:26 +0200

    Added animated, responsive navigation

---
+++ navigation.js
@@ -0,0 +1,21 @@
+const navSlide = () => {
+
+
+const burger = document.querySelector('.navigation__burger');
+const nav = document.querySelector('.navigation__list');
+const navLink = document.querySelectorAll('.navigation__item');
+
+burger.addEventListener('click', () => {
+    nav.classList.toggle('nav-active');
+
+navLink.forEach((link, index) => {
+    if(link.style.animation){
+      link.style.animation ='';
+    }else {
+    link.style.animation =`navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`
+}
+});
+});
+
+}
+navSlide(); 
